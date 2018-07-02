8e66f034a0d5d6564bce879c24f32720a15ec4c3
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-07-02 17:02:35 -0300

    se crea funcion mayor que

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ Sigue los pasos a continuación para que completes el programa:
 
 Retornar el numero mayor entre A, B y C
 Input: A = 10, B = 30, C = 5 Output: 30*/
+function mayorQue (num1, num2, num3){ 
+    if ( num1 > num2 && num1 > num3) {
+        // num1 mayor que num2 y num3
+       }else if (num 2 > num3){
+        // num2 mas grande
+       }else{
+        // num3 seria el mas grande
+       }
+ }
